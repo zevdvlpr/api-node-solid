763140db2ae55d47d3f53262f76fcd02f3bf2fcd
zevdvlpr <zevdvlpr@gmail.com>
2020-09-07 17:02:14 -0300

    app

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter: Mail;
+  
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "5f9b57b9d2fa51",
+        pass: "069816af9e743e"
+      }
+    });
+  }
+  
+  async sendeMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email
+      },
+      subject: message.subject,
+      html: message.body,
+    });
+  }
+}
